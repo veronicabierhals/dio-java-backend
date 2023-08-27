d5af9568090dd5aff407ba73ababed3f5008e24f
Veronica Bierhals <veronicabierhals1@gmail.com>
2023-08-27 09:39:12 -0300

    criado tipos-dados

---
+++ tipos-variaveis/src/tipo-dados.java
@@ -0,0 +1,13 @@
+public public class TipoDados {
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2021;
+
+        //ambos se começarem com zero precisam ser outro tipo, talvez string
+        int cep = 21070333;
+        long cpf = 98765432109L; //precisa ser colocado um 'L' no final, maiúsculo ou minúsculo
+        
+        float pi = 3.14F; //precisa ser colocado um 'F' no final, maiúsculo ou minúsculo
+        double salario = 1275.33;
+    }
+}
