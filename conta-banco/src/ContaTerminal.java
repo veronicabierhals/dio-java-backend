e96a72498e3df9e1e66fcb5c1e52a0eafb473446
Veronica Bierhals <veronicabierhals1@gmail.com>
2023-09-04 13:53:00 -0300

    variaveis declaradas e mensagem final ok

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,11 +1,18 @@
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        //TODO: conhecer e importar a classe scanner
+        int numero = 1021;
+        String agencia = "067-8";
+        String nomeCliente = "MARIO ANDRADE";
+        double saldo = 237.48;
+        // TODO: conhecer e importar a classe scanner
 
-        //exibir as mensagens para o nosso usuário
+        // exibir as mensagens para o nosso usuário
 
-        //obter pela classe scanner os valores digitados no terminal
+        // obter pela classe scanner os valores digitados no terminal
 
-        //exibir a mensagem da conta criada
+        // exibir a mensagem da conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
-}
+};
