c71985e374e8419ba1f115f602d8c7a76ebe76ad
Veronica Bierhals <veronicabierhals1@gmail.com>
2023-09-07 18:35:32 -0300

    Desafio ContaBanco concluído

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,18 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) throws Exception {
-        int numero = 1021;
-        String agencia = "067-8";
-        String nomeCliente = "MARIO ANDRADE";
-        double saldo = 237.48;
-        // TODO: conhecer e importar a classe scanner
 
-        // exibir as mensagens para o nosso usuário
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        // obter pela classe scanner os valores digitados no terminal
+        System.out.println("Por favor, digite o número da Agência:");
+        int numero = scanner.nextInt();
 
-        // exibir a mensagem da conta criada
+        System.out.println("Digite o número da sua conta:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome completo:");
+        scanner.nextLine();
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o valor que deseja depositar:");
+        double saldo = scanner.nextDouble();
+
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
                 + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 };
