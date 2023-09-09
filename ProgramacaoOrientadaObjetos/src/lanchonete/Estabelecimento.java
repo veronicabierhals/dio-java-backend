50a6a13bbeea7feed99473ea4c692513d9002bbf
Veronica Bierhals <veronicabierhals1@gmail.com>
2023-09-09 16:37:02 -0300

    visibilidade de recursos

---
+++ ProgramacaoOrientadaObjetos/src/lanchonete/Estabelecimento.java
@@ -0,0 +1,23 @@
+package lanchonete;
+
+public class Estabelecimento {
+    public static void main(String[] args) {
+        Cozinheiro cozinheiro = new Cozinheiro();
+
+        // ações que estabelecimento precisa ter ciência
+        cozinheiro.adicionarSucoNoBalcao();
+        ;
+        cozinheiro.adicionarLancheNoBalcao();
+        cozinheiro.adicionarComboNoBalcao();
+
+        Atendente atendente = new Atendente();
+        atendente.receberPagamento();
+        atendente.servindoMesa();
+
+        Cliente cliente = new Cliente();
+        cliente.escolherLanche();
+        cliente.fazerPedido();
+        cliente.pagarConta();
+
+    }
+}
